0cefe5bf3a6db35e220f6e47f64af9684b681584
Vaishnavi Madalapure <madalapurevaishnavi@gmail.com>
2024-09-28 16:32:45 +0530

    Add files via upload

---
+++ string/Main151.java
@@ -0,0 +1,32 @@
+package string;
+
+// cd..
+
+//  * javac string/Main151.java
+//  * java string.Main151
+//  */
+
+class Solution {
+    public String reverseWord(String s) {
+        String[] str = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length - 1; i >= 0; i--) {
+            if (!str[i].isEmpty()) {
+                sb.append(str[i]);
+            }
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+}
+
+public class Main151 {
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        System.out.println(obj.reverseWord("hey it    me  vaishnavi "));
+    }
+}
